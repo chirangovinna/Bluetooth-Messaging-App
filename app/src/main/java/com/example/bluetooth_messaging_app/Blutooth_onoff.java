40169011630ca45dd17c6691d7497ef4c5817c35
chirandevinda <e18068@eng.pdn.ac.lk>
2024-02-28 09:20:37 +0530

    initial commit message

---
+++ app/src/main/java/com/example/bluetooth_messaging_app/Blutooth_onoff.java
@@ -0,0 +1,14 @@
+package com.example.bluetooth_messaging_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Blutooth_onoff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blutooth_onoff);
+    }
+}
